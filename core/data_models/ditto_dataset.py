Fix the masking experiment with Ditto and SupCon.

core/data_models/ditto_dataset.py
@@ -4,7 +4,9 @@ Code taken from https://github.com/megagonlabs/ditto/blob/master/ditto_light/dat
 import torch
 from torch.utils.data import Dataset
 from transformers import AutoTokenizer
-from utils.bert_utils import cross_encoder_random_masking, cross_encoder_syntax_masking, cross_encoder_semantic_masking, get_left_right_words_and_ids
+from utils.bert_utils import cross_encoder_random_masking, cross_encoder_syntax_masking, \
+    cross_encoder_semantic_masking, get_left_right_words_and_ids
+from utils.nlp import FastTextModel
 import numpy as np
 
 # map lm name to huggingface's pre-trained model names
@@ -36,7 +38,8 @@ class DittoDataset(Dataset):
                  da=None,
                  typeMask: str = None,
                  topk_mask: int = None,
-                 verbose: bool = False):
+                 verbose: bool = False,
+                 sem_emb_model=None):
         self.tokenizer = get_tokenizer(lm)
         self.pairs = []
         self.labels = []
@@ -58,6 +61,7 @@ class DittoDataset(Dataset):
         self.typeMask = typeMask
         self.topk_mask = topk_mask
         self.verbose = verbose
+        self.sem_emb_model = sem_emb_model
 
     def __len__(self):
         """Return the size of the dataset."""
@@ -104,9 +108,11 @@ class DittoDataset(Dataset):
             )
 
         elif self.typeMask == 'maskSem':
+            assert self.sem_emb_model is not None
             _, _, sent1, sent2 = get_left_right_words_and_ids(self.tokenizer, features)
             features = cross_encoder_semantic_masking(
-                sent1=sent1, sent2=sent2, features=features, topk=self.topk_mask, ignore_tokens=self.stopwords,
+                sent1=sent1, sent2=sent2, features=features, topk=self.topk_mask, sem_emb_model=self.sem_emb_model,
+                ignore_tokens=self.stopwords,
             )
 
         elif self.typeMask is None:

utils/bert_utils.py
@@ -616,7 +616,7 @@ def get_left_right_words_and_ids(tokenizer, features):
     left_words = get_words_from_tokens(left_word_ids, tokens[:sep_ix])
     right_words = get_words_from_tokens(right_word_ids, tokens[sep_ix:])
     assert len(left_words) == left_word_ids[left_word_ids != None].max() + 1
-    assert len(right_words) == right_word_ids[-2][right_word_ids != None].max() + 1
+    assert len(right_words) == right_word_ids[right_word_ids != None].max() + 1
 
     return left_word_ids, right_word_ids, left_words, right_words
 
@@ -635,8 +635,8 @@ def cross_encoder_random_masking(features, topk, tokenizer, ignore_tokens):
     ignore_left = [ix for ix, word in enumerate(left_words) if word in ignore_tokens]
     ignore_right = [ix for ix, word in enumerate(right_words) if word in ignore_tokens]
     # Get the remaining words
-    left_word_ids = np.unique(left_word_ids[~np.isnan(left_word_ids)])
-    right_word_ids = np.unique(right_word_ids[~np.isnan(right_word_ids)])
+    left_word_ids = np.unique(left_word_ids[left_word_ids != None])
+    right_word_ids = np.unique(right_word_ids[right_word_ids != None])
     assert len(left_word_ids) == len(left_words)
     assert len(right_word_ids) == len(right_words)
     left_word_ids = left_word_ids[~np.isin(left_word_ids, ignore_left)]
@@ -646,10 +646,10 @@ def cross_encoder_random_masking(features, topk, tokenizer, ignore_tokens):
     mask_indices = get_random_pair_indices(left_word_ids, right_word_ids, topk)
 
     # Mask the words associated to the selected indices
-    input_ids = features['input_ids'].unsqueeze(0)
+    input_ids = features['input_ids']
     for pair in mask_indices:
         index_mask = get_index_token_sent(pair, word_ids)
-        input_ids[0][0, index_mask] = 103
+        input_ids[0, index_mask] = 103
 
     features['inputs_ids'] = input_ids
 
@@ -741,10 +741,10 @@ def cross_encoder_syntax_masking(sent1, sent2, features, topk, ignore_tokens=Non
         order = np.array(top_word_pairs_by_syntax['word_pair_sims']).argsort()
         top_word_pairs_by_syntax = {k: np.array(v)[order][:topk] for k, v in top_word_pairs_by_syntax.items()}
 
-        input_ids = features['input_ids'].unsqueeze(0)
+        input_ids = features['input_ids']
         for couple in top_word_pairs_by_syntax['word_pair_idxs']:
             index_mask = get_index_token_sent(couple, features.word_ids())
-            input_ids[0][0, index_mask] = 103
+            input_ids[0, index_mask] = 103
 
         features['inputs_ids'] = input_ids
     return features
@@ -781,13 +781,7 @@ def bi_encoder_syntax_masking(sent1, sent2, left_features, right_features, topk,
     return left_input_ids, right_input_ids
 
 
-def cross_encoder_semantic_masking(sent1, sent2, features, topk, ignore_tokens=None):
-    if os.path.isfile('modelPick.pkl'):
-        sem_emb_model = pickle.load(open("modelPick.pkl", "rb"))
-    else:
-        FAST_TEXT_PATH = os.path.join(os.path.abspath('../..'), 'data', 'wiki-news-300d-1M', 'wiki-news-300d-1M.vec')
-        sem_emb_model = gensim.models.KeyedVectors.load_word2vec_format(FAST_TEXT_PATH, binary=False, encoding='utf8')
-        pickle.dump(sem_emb_model, open("modelPick.pkl", "wb"))
+def cross_encoder_semantic_masking(sent1, sent2, features, topk, sem_emb_model, ignore_tokens=None):
 
     if ignore_tokens is None:
         ignore_tokens = []
@@ -801,23 +795,17 @@ def cross_encoder_semantic_masking(sent1, sent2, features, topk, ignore_tokens=N
         order = np.array(top_word_pairs_by_semantic['word_pair_sims']).argsort()[::-1]
         top_word_pairs_by_semantic = {k: np.array(v)[order][:topk] for k, v in top_word_pairs_by_semantic.items()}
 
-        input_ids = features['input_ids'].unsqueeze(0)
+        input_ids = features['input_ids']
         for couple in top_word_pairs_by_semantic['word_pair_idxs']:
             index_mask = get_index_token_sent(couple, features.word_ids())
-            input_ids[0][0, index_mask] = 103
+            input_ids[0, index_mask] = 103
 
         features['inputs_ids'] = input_ids
     return features
 
 
-def bi_encoder_semantic_masking(sent1, sent2, left_features, right_features, topk, ignore_tokens=None, index=0):
-    if os.path.isfile('modelPick.pkl'):
-        sem_emb_model = pickle.load(open("modelPick.pkl", "rb"))
-    else:
-        FAST_TEXT_PATH = os.path.join(os.path.abspath('../..'), 'data', 'wiki-news-300d-1M', 'wiki-news-300d-1M.vec')
-        sem_emb_model = gensim.models.KeyedVectors.load_word2vec_format(FAST_TEXT_PATH, binary=False, encoding='utf8')
-        pickle.dump(sem_emb_model, open("modelPick.pkl", "wb"))
-
+def bi_encoder_semantic_masking(sent1, sent2, left_features, right_features, topk, sem_emb_model, ignore_tokens=None,
+                                index=0):
     if ignore_tokens is None:
         ignore_tokens = []
     ignore_tokens += ['[SEP]']

utils/masking/word_masking.py
@@ -19,6 +19,8 @@ from utils.ditto_utils import tune_threshold, predict
 from core.modeling.supcon import ContrastiveClassifierModel
 from torch.utils.data import DataLoader
 from utils.supcon_utils import DataCollatorContrastiveClassification
+from utils.nlp import FastTextModel
+from sklearn.metrics import f1_score
 
 PROJECT_DIR = Path(__file__).parent.parent.parent
 RESULTS_DIR = os.path.join(PROJECT_DIR, 'results', 'models')
@@ -193,11 +195,20 @@ def evaluate_ditto(use_case: str, lm: str, type_mask: str, topk_mask: int, max_l
     valid_path = injector.transform_file(valid_path)
     test_path = injector.transform_file(test_path)
 
+    if type_mask == 'maskSem':
+        print("Loading the FastText model!")
+        sem_emb_model = FastTextModel()
+        print("Loading completed!")
+    else:
+        sem_emb_model = None
+
     valid_dataset = DittoDataset(
-        path=valid_path, lm=lm, typeMask=type_mask, topk_mask=topk_mask, max_len=max_len, verbose=False
+        path=valid_path, lm=lm, typeMask=type_mask, topk_mask=topk_mask,
+        max_len=max_len, verbose=False, sem_emb_model=sem_emb_model
     )
     test_dataset = DittoDataset(
-        path=test_path, lm=lm, typeMask=type_mask, topk_mask=topk_mask, max_len=max_len, verbose=True
+        path=test_path, lm=lm, typeMask=type_mask, topk_mask=topk_mask,
+        max_len=max_len, verbose=True, sem_emb_model=sem_emb_model
     )
 
     model_path = os.path.join(RESULTS_DIR, "ditto", f"{use_case}.pt")
@@ -208,13 +219,15 @@ def evaluate_ditto(use_case: str, lm: str, type_mask: str, topk_mask: int, max_l
 
     res = evaluate(tuned_model, test_dataset, thr=threshold, collate_fn=test_dataset.pad)
 
-    # from sklearn.metrics import f1_score
-    # print(f1_score(res['labels'], res['preds']))
+    print(f1_score(res['labels'], res['preds']))
+
+    if sem_emb_model is not None:
+        sem_emb_model.save_model_with_cache()
 
     return res
 
 
-def evaluate_supcon(use_case: str, lm: str, type_mask: str, topk_mask: int):
+def evaluate_supcon(use_case: str, lm: str, type_mask: str, topk_mask: int, max_len: int):
     def get_posneg():
         if lm == ['Structured_Walmart-Amazon', 'Dirty_Walmart-Amazon']:
             return 10
@@ -245,13 +258,23 @@ def evaluate_supcon(use_case: str, lm: str, type_mask: str, topk_mask: int):
         device='cpu'
     )
 
+    if type_mask == 'maskSem':
+        print("Loading the FastText model!")
+        sem_emb_model = FastTextModel()
+        print("Loading completed!")
+    else:
+        sem_emb_model = None
+
     data_collator = DataCollatorContrastiveClassification(
-        tokenizer=test_dataset.tokenizer, typeMask=type_mask, topk_mask=topk_mask
+        tokenizer=test_dataset.tokenizer, typeMask=type_mask, topk_mask=topk_mask,
+        max_length=max_len, sem_emb_model=sem_emb_model
     )
     res = evaluate(tuned_model, test_dataset, collate_fn=data_collator)
 
-    # from sklearn.metrics import f1_score
-    # print(f1_score(res['labels'], res['preds']))
+    print(f1_score(res['labels'], res['preds']))
+
+    if sem_emb_model is not None:
+        sem_emb_model.save_model_with_cache()
 
     return res
 
@@ -297,7 +320,7 @@ if __name__ == '__main__':
 
     for use_case in use_cases:
         for token in ['sent_pair']:  # 'attr_pair', 'sent_pair'
-            for modeMask in ['maskSyn']:  # 'off', 'random', 'maskSem', 'maskSyn'
+            for modeMask in ['maskSem', 'maskSyn']:  # 'off', 'random', 'maskSem', 'maskSyn'
                 for topk_mask in [3]:  # None, 3
                     if modeMask == 'selectCol' and token == 'sent_pair':
                         continue
@@ -312,7 +335,8 @@ if __name__ == '__main__':
 
                     elif args.approach == 'supcon':
                         res = evaluate_supcon(
-                            use_case=use_case, lm=args.bert_model, type_mask=modeMask, topk_mask=topk_mask
+                            use_case=use_case, lm=args.bert_model, type_mask=modeMask, topk_mask=topk_mask,
+                            max_len=args.max_len
                         )
 
                     else:

utils/supcon_utils.py
@@ -9,11 +9,12 @@ import random
 
 random.seed(42)
 from dataclasses import dataclass
-from typing import Optional, List
+from typing import Optional, Any
 import torch
 from transformers.tokenization_utils_base import PreTrainedTokenizerBase
 from utils.bert_utils import bi_encoder_random_masking, bi_encoder_syntax_masking, bi_encoder_semantic_masking, \
     get_words_from_tokens
+from utils.nlp import FastTextModel
 
 
 @dataclass
@@ -24,6 +25,7 @@ class DataCollatorContrastiveClassification:
     return_tensors: str = "pt"
     typeMask: Optional[str] = None
     topk_mask: Optional[int] = None
+    sem_emb_model: Optional[Any] = None
 
     stopwords = ['COL', 'VAL', '[COL]', '[VAL]', 'PERSON', 'ORG', 'LOC', 'PRODUCT', 'DATE', 'QUANTITY', 'TIME',
                  'Artist_Name', 'name', 'Released', 'CopyRight', 'content', 'Brew_Factory_Name', 'Time', 'type',
@@ -71,6 +73,7 @@ class DataCollatorContrastiveClassification:
             batch_right['input_ids'] = torch.cat([x[1].unsqueeze(0) for x in features])
 
         elif self.typeMask == 'maskSem':
+            assert self.sem_emb_model is not None
             sent1_list = []
             sent2_list = []
             for i in range(len(batch_left['input_ids'])):
@@ -85,7 +88,8 @@ class DataCollatorContrastiveClassification:
 
             features = [bi_encoder_semantic_masking(
                 sent1=sent1_list[i], sent2=sent2_list[i], left_features=batch_left,
-                right_features=batch_right, topk=self.topk_mask, ignore_tokens=self.stopwords, index=i
+                right_features=batch_right, topk=self.topk_mask, sem_emb_model=self.sem_emb_model,
+                ignore_tokens=self.stopwords, index=i
             ) for i in range(len(batch_left['input_ids']))]
             batch_left['input_ids'] = torch.cat([x[0].unsqueeze(0) for x in features])
             batch_right['input_ids'] = torch.cat([x[1].unsqueeze(0) for x in features])